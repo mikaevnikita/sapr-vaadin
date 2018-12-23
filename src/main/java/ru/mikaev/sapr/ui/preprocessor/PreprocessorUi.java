250f3db5779a4cacd3363c9cf8ba0931b7311711
Nikita Mikaev <mikaev.nikita@accenture.com>
2018-12-23 22:47:30 +0300

    + edit for rod and knot

--- src/main/java/ru/mikaev/sapr/ui/preprocessor/PreprocessorUi.java
+++ src/main/java/ru/mikaev/sapr/ui/preprocessor/PreprocessorUi.java
@@ -4,14 +4,17 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
+import org.apache.commons.lang3.StringUtils;
 import ru.mikaev.sapr.common.PreprocessorDataHolder;
 import ru.mikaev.sapr.dto.ConstructionDto;
 import ru.mikaev.sapr.dto.KnotDto;
@@ -53,6 +56,21 @@ public class PreprocessorUi
 
     private final Checkbox rightSupportCheckbox;
 
+
+    //rod grid operations panel
+    private final Button editRodButton;
+
+    private final HorizontalLayout editRodLayout;
+    private final TextField rodL;
+    private final TextField rodA;
+    private final TextField rodE;
+    private final TextField rodSigma;
+    private final TextField rodLoad;
+
+    //knot grid operations panel
+    private final TextField knotLoadField;
+    private final Button editKnotButton;
+
     public PreprocessorUi(PreprocessorDataService dataService,
                           PreprocessorDataMapper mapper,
                           PreprocessorDataHolder holder,
@@ -63,6 +81,53 @@ public class PreprocessorUi
         this.rodMapper = rodMapper;
 
         datasetNameH2 = new H2();
+
+        //rod
+        editRodButton = new Button("Edit rod");
+        editRodButton.addClickListener(e -> onEditRod());
+
+        editRodLayout = new HorizontalLayout();
+
+        rodL = new TextField("L");
+        rodL.setPattern("[0-9]*");
+        rodL.setPreventInvalidInput(true);
+        rodL.addValueChangeListener(e -> onRodTextFieldChanged(e.getOldValue(), e.getValue(), e.getSource()));
+
+        rodA = new TextField("A");
+        rodA.setPattern("[0-9]*");
+        rodA.setPreventInvalidInput(true);
+        rodA.addValueChangeListener(e -> onRodTextFieldChanged(e.getOldValue(), e.getValue(), e.getSource()));
+
+
+        rodE = new TextField("E");
+        rodE.setPattern("[0-9]*");
+        rodE.setPreventInvalidInput(true);
+        rodE.addValueChangeListener(e -> onRodTextFieldChanged(e.getOldValue(), e.getValue(), e.getSource()));
+
+
+        rodSigma = new TextField("σ");
+        rodSigma.setPattern("[0-9]*");
+        rodSigma.setPreventInvalidInput(true);
+        rodSigma.addValueChangeListener(e -> onRodTextFieldChanged(e.getOldValue(), e.getValue(), e.getSource()));
+
+
+        rodLoad = new TextField("Load");
+        rodLoad.setPattern("-?[0-9]*");
+        rodLoad.setPreventInvalidInput(true);
+        rodLoad.addValueChangeListener(e -> onRodTextFieldChanged(e.getOldValue(), e.getValue(), e.getSource()));
+
+
+        editRodLayout.add(rodL, rodA, rodE, rodSigma, rodLoad);
+
+        editRodLayout.setSizeFull();
+        editRodLayout.setVisible(false);
+
+        //knot
+        knotLoadField = new TextField();
+        knotLoadField.setPlaceholder("Load");
+        knotLoadField.setPattern("-?[0-9]*");
+        knotLoadField.setPreventInvalidInput(true);
+        editKnotButton = new Button("Edit knot");
 
         leftSupportCheckbox = new Checkbox("Left support");
         leftSupportCheckbox.addValueChangeListener(e -> onLeftSupportChanged(e.getOldValue(), e.getValue()));
@@ -86,17 +151,9 @@ public class PreprocessorUi
 
         HorizontalLayout supports = getSupports();
 
-        HorizontalLayout gridLayout = new HorizontalLayout();
+        VerticalLayout gridLayout = new VerticalLayout();
 
-        VerticalLayout rodGridLayout = new VerticalLayout();
-        rodGridLayout.add(new H2("Rods"), rodGrid);
-        rodGridLayout.setSizeFull();
-
-        VerticalLayout knotGridLayout = new VerticalLayout();
-        knotGridLayout.add(new H2("Knots"), knotGrid);
-        knotGridLayout.setSizeFull();
-
-        gridLayout.add(rodGridLayout, knotGridLayout);
+        gridLayout.add(getRodLayout(), getKnotLayout());
         gridLayout.setSizeFull();
 
 
@@ -108,6 +165,41 @@ public class PreprocessorUi
         updateRodGrid();
         updateKnotGrid();
         updateSupports();
+    }
+
+    private void onRodTextFieldChanged(String oldValue, String newValue, TextField source) {
+        if(StringUtils.isBlank(newValue)){
+            source.setValue("0");
+        }
+    }
+
+    private VerticalLayout getRodLayout(){
+        VerticalLayout rodGridLayout = new VerticalLayout();
+
+        HorizontalLayout rodGridOperationsPanel = new HorizontalLayout();
+        rodGridOperationsPanel.add(new Button("Add rod", event -> addRod()));
+        rodGridOperationsPanel.add(new Button("Delete rod", event -> deleteRod()));
+        rodGridOperationsPanel.add(editRodButton);
+
+        rodGridOperationsPanel.setSizeFull();
+
+        rodGridLayout.add(new H2("Rods"), rodGridOperationsPanel, editRodLayout, rodGrid);
+        rodGridLayout.setSizeFull();
+
+        return rodGridLayout;
+    }
+
+    private VerticalLayout getKnotLayout(){
+        VerticalLayout knotGridLayout = new VerticalLayout();
+
+        HorizontalLayout knotGridOperationsPanel = new HorizontalLayout();
+        knotGridOperationsPanel.add(knotLoadField, editKnotButton);
+        knotGridOperationsPanel.setSizeFull();
+
+        knotGridLayout.add(new H2("Knots"), knotGridOperationsPanel, knotGrid);
+        knotGridLayout.setSizeFull();
+
+        return knotGridLayout;
     }
 
     private void updateRodGrid() {
@@ -157,12 +249,34 @@ public class PreprocessorUi
     private HorizontalLayout getMenu() {
         HorizontalLayout menu = new HorizontalLayout();
         menu.add(new Button("Previous page", event -> getUI().ifPresent(ui -> ui.navigate("repository"))));
-        menu.add(new Button("Add rod", event -> addRod()));
-        menu.add(new Button("Delete rod", event -> deleteRod()));
-        menu.add(new Button("Save", event -> saveData()));
-        menu.add(new Button("Draw", event -> Notification.show("Draw construction")));
+        menu.add(new Button("Save construction", event -> saveData()));
+        menu.add(new Button("Draw construction", event -> Notification.show("Draw construction")));
 
         return menu;
+    }
+
+    private void onEditRod(){
+        if(editRodButton.getText().equals("Save rod")){
+            editRodLayout.setVisible(false);
+            editRodButton.setText("Edit rod");
+        }
+        else{
+            if(rodGrid.getSelectionModel().getFirstSelectedItem().isPresent()){
+                editRodLayout.setVisible(true);
+                editRodButton.setText("Save rod");
+
+                final RodDto selectedRod = rodGrid.getSelectionModel().getFirstSelectedItem().get();
+
+                rodA.setValue(String.valueOf(selectedRod.getA()));
+                rodL.setValue(String.valueOf(selectedRod.getL()));
+                rodE.setValue(String.valueOf(selectedRod.getE()));
+                rodSigma.setValue(String.valueOf(selectedRod.getSigma()));
+                rodLoad.setValue(String.valueOf(selectedRod.getLoad()));
+            }
+            else{
+                Notification.show("You must select a rod!");
+            }
+        }
     }
 
 
@@ -188,7 +302,7 @@ public class PreprocessorUi
         final List<RodDto> rods = holder.getPreprocessorData().get().getConstruction().getRods();
 
         if(rods.size() == 1){
-            Notification.show("Хотя бы один стержень должен быть!");
+            Notification.show("At least one rod must be!");
         }
         else{
             rods.remove(rods.size() - 1);
