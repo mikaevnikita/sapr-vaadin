3f3e85eaa18415c848a79accf5401b6d0d4f54f9
Nikita Mikaev <mikaev.nikita@accenture.com>
2018-12-26 21:49:55 +0300

    + processor data holder

---
+++ src/main/java/ru/mikaev/sapr/common/ProcessorDataHolder.java
@@ -0,0 +1,14 @@
+package ru.mikaev.sapr.common;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import ru.mikaev.sapr.dto.ProcessorResult;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@Data
+public class ProcessorDataHolder {
+    private Optional<ProcessorResult> processorResult;
+}
